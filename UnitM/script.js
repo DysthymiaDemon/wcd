c1d59d32e540936c7023d18a3bab4e82ea440987
MyridianStar <38551387+MyridianStar@users.noreply.github.com>
2018-07-20 16:34:02 +0800

    aaaaaaaa

--- UnitM/script.js
+++ UnitM/script.js
@@ -6,12 +6,23 @@
    Date:     19/07/2018
    HTML5 and CSS3 Illustrated Unit M, Visual Workshop
  */
+console.log("script.js now running...");
 /* create variables */
 var smallbutton = document.querySelector(".smaller-size");
+var largebutton = document.querySelector(".larger-size");
+var defaultbutton = document.querySelector(".selected-button");
 var applyall = document.querySelector("html");
 
 function fontsmaller() {
   applyall.className = "smaller-size";
 }
+function fontdefault() {
+  applyall.className = "selected-button";
+}
+function fontbigger() {
+  applyall.className = "larger-size";
+}
 
 smallbutton.addEventListener("click",fontsmaller);
+largebutton.addEventListener("click",fontbigger);
+defaultbutton.addEventListener("click",fontdefault);
