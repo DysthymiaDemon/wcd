757cd58efdcc7603d8d62ed40cd3091cf10ea052
MyridianStar <38551387+MyridianStar@users.noreply.github.com>
2018-07-20 15:39:17 +0800

    just do it

--- UnitM/script.js
+++ UnitM/script.js
@@ -6,4 +6,14 @@
    Date:     19/07/2018
    HTML5 and CSS3 Illustrated Unit M, Visual Workshop
  */
+/* create variables */
+var smallbutton = document.querySelector(".smaller-size");
+var applyall = document.querySelector("html");
 
+makeItSmaller.className = "html .smallersize";
+
+applyall.addEventListener("click", fontsmaller, false);
+
+function fontsmaller() {
+  applyall.className = ("smallbutton");
+}
