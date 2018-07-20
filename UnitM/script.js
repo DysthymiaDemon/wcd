749221b2495e0b51ccfb93d24de9020d88d9a905
MyridianStar <38551387+MyridianStar@users.noreply.github.com>
2018-07-21 00:36:59 +0800

    The End?

--- UnitM/script.js
+++ UnitM/script.js
@@ -13,16 +13,40 @@ var largebutton = document.querySelector(".larger-size");
 var defaultbutton = document.querySelector(".selected-button");
 var applyall = document.querySelector("html");
 
-function fontsmaller() {
+function fontsmaller(){
   applyall.className = "smaller-size";
 }
-function fontdefault() {
-  applyall.className = "selected-button";
+function fontdefault(){
+  applyall.className = "default-size";
 }
-function fontbigger() {
+function fontbigger(){
   applyall.className = "larger-size";
+}
+function idlesmall(){
+  smallbutton.className = "selected-button smaller-size";
+}
+function hoversmall(){
+  smallbutton.className = "selected-button smaller-size whiteout";
+}
+function idlemiddle(){
+  defaultbutton.className = "selected-button";
+}
+function hovermiddle(){
+  defaultbutton.className = "selected-button whiteout";
+}
+function idlelarge(){
+  largebutton.className = "selected-button larger-size";
+}
+function hoverlarge(){
+  largebutton.className = "selected-button whiteout";
 }
 
 smallbutton.addEventListener("click",fontsmaller);
-largebutton.addEventListener("click",fontbigger);
 defaultbutton.addEventListener("click",fontdefault);
+largebutton.addEventListener("click",fontbigger);
+smallbutton.addEventListener("mouseover",hoversmall);
+smallbutton.addEventListener("mouseout",idlesmall);
+defaultbutton.addEventListener("mouseover", hovermiddle);
+defaultbutton.addEventListener("mouseout",idlemiddle);
+largebutton.addEventListener("mouseover",hoverlarge);
+largebutton.addEventListener("mouseout",idlelarge);
