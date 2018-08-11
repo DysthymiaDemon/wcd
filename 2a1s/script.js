d5c20c62138ac3721f0ea3b996697ca956248d9b
MyridianStar <38551387+MyridianStar@users.noreply.github.com>
2018-08-11 15:05:07 +0800

    .

--- 2a1s/script.js
+++ 2a1s/script.js
@@ -13,3 +13,12 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
