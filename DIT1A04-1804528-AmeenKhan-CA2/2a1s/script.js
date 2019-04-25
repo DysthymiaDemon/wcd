713bdbc2935fcd42126d6f745d2eaf75e7fdd742
Ameen Khan <ameenkhan1208@gmail.com>
2019-04-25 10:50:23 +0800

    return to github
    ah shit, here we go again.

---
+++ DIT1A04-1804528-AmeenKhan-CA2/2a1s/script.js
@@ -0,0 +1,12 @@
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+window.onscroll = function() {myFunction()};
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
